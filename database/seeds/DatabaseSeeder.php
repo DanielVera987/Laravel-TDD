722680926df1cd5f21a15e6b52af28f301a083c2
danielvera987 <danielveraangulo703@gmail.com>
2020-12-15 21:22:47 -0500

    insert seed test

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->truncateTables([
+            'profession',
+            'users'
+        ]);
+
+        $this->call(ProfessionSeeder::class);
+        $this->call(UserSeeder::class);
+
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1;'); // Reactivamos la revisión de claves foráneas
+    }
+
+    public function truncateTables(array $tables) {
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0;'); // Desactivamos la revisión de claves foráneas
+
+        foreach($tables as $table) {
+            DB::table($table)->truncate();
+        }
     }
 }
